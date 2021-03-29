2c85f41e3e9370b78388942cbddd1477ff98940a
Anna Voleníková <anna.volenikova@gmail.com>
2021-03-29 10:37:45 +0200

    Update PBS_Metacentrum_NanoPlot.sh

--- PBS_Metacentrum_NanoPlot.sh
+++ PBS_Metacentrum_NanoPlot.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #PBS -l walltime=12:00:00
 #PBS -l select=1:ncpus=8:mem=32gb:scratch_local=500gb
+#PBS -N NanoPlot
+#PBS -o /PATH/to/output/directory/NanoPlot.stdout
+#PBS -e /PATH/to/output/directory/NanoPlot.stderr
 
 trap 'clean_scratch' TERM EXIT
 cd $SCRATCHDIR || exit 1
