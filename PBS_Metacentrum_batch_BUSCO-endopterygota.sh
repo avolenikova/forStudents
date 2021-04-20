d8524e228cf890964f93a4675f24dcb7b9ecdaaf
Anna Volenikova <volen_a@nympha.zcu.cz>
2021-04-20 10:36:54 +0200

    Batch PBS scripts for BUSCO

---
+++ PBS_Metacentrum_batch_BUSCO-endopterygota.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#usage: $ bash ThisScript.sh /PATH/to/input/dir/ /PATH/to/output/dir/
+#1) PREPARE ENVIRONMENT
+inputDir=$1
+timeStamp=$(date +"%Y%m%d.%H%M")
+outDir=${2}BUSCO_endo_${timeStamp}
+mkdir $outDir
+cd $outDir
+#2) CHOOSE DATA AND PRINT PBS SCRIPT
+for file in "${1}"*.fasta.gz
+do
+	fileName="$(basename "$file" .fasta.gz)"
+	baseName="$(basename "$file")"
+	scriptName=${outDir}/BUSCO_endo_"${fileName}".pbs
+	(
+	cat << endOfPBSscript
+#!/bin/bash
+#PBS -N BUSCO-endo
+#PBS -l select=1:ncpus=2:mem=32gb:scratch_local=250gb
+#PBS -l walltime=71:00:00
+#PBS -o ${outDir}/${fileName}_BUSCO-endo.stdout
+#PBS -e ${outDir}/${fileName}_BUSCO-endo.stderr
+
+trap 'clean_scratch' TERM EXIT
+
+scp ${file} \${SCRATCHDIR} || exit 1
+cp -r /software/busco/3.0.2/src/db/endopterygota_odb9/ \${SCRATCHDIR} || exit 1
+cp -r /software/augustus/3.3.1/src/config \${SCRATCHDIR} || exit 1
+
+cd \${SCRATCHDIR} || exit 2
+
+chmod -R 777 \${SCRATCHDIR}/config
+
+gunzip ${baseName}
+
+module add busco-3.0.2
+export AUGUSTUS_CONFIG_PATH=\${SCRATCHDIR}/config
+
+run_BUSCO.py -c 1 -i \${SCRATCHDIR}/${fileName}.fasta -o ${fileName}_endo_busco.out -l \${SCRATCHDIR}/endopterygota_odb9/ -m geno
+
+rm \${SCRATCHDIR}/${fileName}.fasta
+
+scp -r \${SCRATCHDIR}/* ${outDir} || export CLEAN_SCRATCH=true
+
+endOfPBSscript
+
+	) > ${scriptName}
+	chmod +x ${scriptName}
+	qsub ${scriptName}
+done
